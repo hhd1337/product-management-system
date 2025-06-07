3422aba6e1faefbe20eff9399810947313954210
goeka <goeka1337@gmail.com>
2025-06-08 08:52:25 +0900

    [#5] Feat : ProductCreateDTO 폼 빈 만들고 상품 등록/수정 시 유효성 검사 추가함.

--- src/main/java/kr/ac/hansung/cse/hellospringdatajpa/controller/ProductController.java
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/controller/ProductController.java
@@ -1,10 +1,13 @@
 package kr.ac.hansung.cse.hellospringdatajpa.controller;
 
+import jakarta.validation.Valid;
+import kr.ac.hansung.cse.hellospringdatajpa.dto.ProductCreateDTO;
 import kr.ac.hansung.cse.hellospringdatajpa.entity.Product;
 import kr.ac.hansung.cse.hellospringdatajpa.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,14 +28,20 @@ public class ProductController {
         return "index";
     }
 
-    @GetMapping("/new")
+    /*@GetMapping("/new")
     public String showNewProductPage(Model model) {
 
         Product product = new Product();
         model.addAttribute("product", product);
 
         return "new_product";
+    }*/
+    @GetMapping("/new")
+    public String showNewProductPage(Model model) {
+        model.addAttribute("product", new ProductCreateDTO()); // 이름은 "product" 유지
+        return "new_product";
     }
+
 
     @GetMapping("/edit/{id}")
     public String showEditProductPage(@PathVariable(name = "id") Long id, Model model) {
@@ -46,13 +55,32 @@ public class ProductController {
     // @ModelAttribute는  Form data (예: name=Laptop&brand=Samsung&madeIn=Korea&price=1000.00)를 Product 객체
     // @RequestBody는 HTTP 요청 본문에 포함된
     //  JSON 데이터(예: {"name": "Laptop", "brand": "Samsung", "madeIn": "Korea", "price": 1000.00})를 Product 객체에 매핑
-    @PostMapping("/save")
+    /*@PostMapping("/save")
     public String saveProduct(@ModelAttribute("product") Product product) {
 
         service.save(product);
 
         return "redirect:/products";
+    }*/
+    @PostMapping("/save")
+    public String saveProduct(@ModelAttribute("product") @Valid ProductCreateDTO productForm,
+                              BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "new_product"; // 또는 edit_product: 상황에 맞게 조정
+        }
+
+        Product product = new Product(
+                productForm.getName(),
+                productForm.getBrand(),
+                productForm.getMadeIn(),
+                productForm.getPrice()
+        );
+
+        service.save(product);
+        return "redirect:/products";
     }
+
 
     @GetMapping("/delete/{id}")
     public String deleteProduct(@PathVariable(name = "id") Long id) {
